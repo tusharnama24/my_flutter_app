9ddba4bcc3f9078a4980de1b57189e6aafd870dc
Tushar Nama <tusharnama844@gmail.com>
2025-12-17 15:27:30 +0530

    Fix Flutter PATH issue on Netlify

--- bash.sh
+++ bash.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+set -e
+
+# Install Flutter
 git clone https://github.com/flutter/flutter.git -b stable
-export PATH="$PATH:`pwd`/flutter/bin"
+
+# Add Flutter to PATH (important)
+export PATH="$PATH:$PWD/flutter/bin"
+
+# Verify Flutter
 flutter doctor
+
+# Get packages
 flutter pub get
+
+# Build web
+flutter build web
